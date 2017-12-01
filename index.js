be1ab60674d4542783aff1ec72ef59bae0936809
Liam <creatiionhxx@gmail.com>
2017-12-01 19:39:20 +0000

    Cleaned up some code - stop throwing errors.

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
 const electron = require('electron');
-const { app, BrowserWindow, globalShortcut } = electron;
+const {
+	app,
+	BrowserWindow,
+	globalShortcut
+} = electron;
 let win;
-
 app.on('ready', function() {
-    win = new BrowserWindow({ show: false });
-    win.maximize();
-    win.loadURL('file://' + __dirname + '/index.html');
-    win.webContents.on('did-finish-load', function() {
-        win.show();
-        win.setMenu(null);
-    });
-    globalShortcut.register("CommandOrControl+Q", function() {
-        app.quit();
-        app.exit();
-    })
+	win = new BrowserWindow({
+		show: false
+	});
+	win.maximize();
+	win.loadURL('file://' + __dirname + '/index.html');
+	win.setMenu(null);
+	win.webContents.on('did-finish-load', function() {
+		win.show();
+	});
+	globalShortcut.register("CommandOrControl+Q", function() {
+		app.quit();
+		app.exit();
+	})
 })
 app.on('close', function() {
-    app.quit();
-    app.exit();
+	app.quit();
+	app.exit();
 });
--- stats.js
+++ stats.js
@@ -1,4 +1,5 @@
 const request = require('request');
+const fs = require('fs');
 
 function getStats(game, gamertag, platform) {
 	var settings = {
@@ -51,12 +52,14 @@ function getStats(game, gamertag, platform) {
 				$('.panel').fadeIn();
 				$('.error').fadeOut();
 				$('.mp').fadeIn();
-			} else $('.error').fadeIn();
-			var html = '<div class="panel_red">' + '<h2> Unexpected Error. </h2>' + '<p> User Cannot be found. </p>' + '<p> Try another search query. </p>' + '</div>';
-			$('.error').html(html);
+			} else {
+				$('.error').fadeIn();
+				var html = '<div class="panel_red">' + '<h2> Unexpected Error. </h2>' + '<p> User Cannot be found. </p>' + '<p> Try another search query. </p>' + '</div>';
+				$('.error').html(html);
+			}
 		});
 	} catch (exception) {
-		return null;
+		
 	}
 }
 
@@ -88,40 +91,54 @@ function checkStats(game, platform, page) {
 					$('.tbody').append("<tr><td>" + info.data.entries[i].rank + "</td><td>" + info.data.entries[i].username + "</td><td>" + info.data.entries[i].values.totalXp + "</td><td>" + info.data.entries[i].values.score + "</td><td>" + info.data.entries[i].values.gamesPlayed + "</td><td>" + secondsToHms(info.data.entries[i].values.timePlayed) + "</td></tr>")
 				};
 			} catch (exception) {
-				return null;
 			}
 		});
 	} catch (exception) {
-		return;
+		
 	}
 }
 
-function getCSRF() {  
-	var url = {    
+function getData() {
+	var url = {
 		method: 'GET',
-		    uri: "https://profile.callofduty.com/",
-		    headers: {      
-			'Content-Type': 'application/json'    
+		uri: "https://profile.callofduty.com/cod/userInfo/MTQ0OTE2NTk5NDg2MjE1ODcwMDI6MTUxMzI0ODY5NTA1MTplZDBkYzcwOTg3Y2M2MDcwNWJhMDBlMDY2YTk5MDJjNA",
+		headers: {
+			'Content-Type': 'application/json',
+			'Cookie': 'AMCV_0FB367C2524450B90A490D4C%40AdobeOrg=1099438348%7CMCIDTS%7C17501%7CMCMID%7C06274282484548674580096429307447731106%7CMCAAMLH-1512636251%7C6%7CMCAAMB-1512643887%7CRKhpRz8krg2tLO6pguXWp5olkAcUniQYPHaMWWgdJ3xzPWQmdj0y%7CMCOPTOUT-1512046287s%7CNONE%7CMCAID%7CNONE%7CMCSYNCSOP%7C411-17506%7CvVersion%7C2.1.0; mbox=PC#7b3f20e658be47a393e4606c18564e9b.26_14#1519815088|session#a5b8dde9ef63450b8abdc7c55440c6dc#1512040948|check#true#1512039148; s_nr=1512039089070-Repeat; ACT_SSO_REMEMBER_ME=MTQ0OTE2NTk5NDg2MjE1ODcwMDI6MTZmNDhlYzg2NzRkMzkxNmIxNzQyZmM4OWNjY2NlMDM0ZTU3MTdkNDFhYjZjYjZiZjU4YzY0ZGYwYzU0N2E3MQ; CRM_BLOB=eyJ2ZXIiOjEsInBsYXQiOnt9fQ; __utma=229673283.361720785.1511957272.1511967999.1512031449.5; __utmz=229673283.1511957272.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); ATVI_VISITOR_ID=anon-1511959111787-0.8013522209195922; atvi-cookie=true; AMCVS_0FB367C2524450B90A490D4C%40AdobeOrg=1; ACT_SSO_LOCALE=en_US; __utmc=229673283; s_ppvl=https%253A%2F%2Fprofile.callofduty.com%2Fcod%2Flogin%2C74%2C74%2C978%2C1920%2C978%2C1920%2C1080%2C1%2CL; s_ppv=https%253A%2F%2Fprofile.callofduty.com%2Fcod%2Flogin%253FredirectUrl%253Dhttps%25253A%25252F%25252Fmy.callofduty.com%25252Fwwii%25252Fstats%25252Flifetime%2C74%2C74%2C978%2C1920%2C978%2C1920%2C1080%2C1%2CL; s_cc=true; s_sq=%5B%5BB%5D%5D; new_SiteId=cod; comid=cod; XSRF-TOKEN=9e9d114c-c67a-443d-92ca-c5273789dc84; s_dfa=activision.prd; gpv_c8=no%20value; redirectUrl=https://my.callofduty.com/wwii/stats/lifetime; SSO_REDIRECTED_AT_LOGIN=https://my.callofduty.com/wwii/stats/lifetime; ACT_SSO_COOKIE=MTQ0OTE2NTk5NDg2MjE1ODcwMDI6MTUxMzI0ODY5NTA1MTplZDBkYzcwOTg3Y2M2MDcwNWJhMDBlMDY2YTk5MDJjNA; s_ACT_SSO_COOKIE=MTQ0OTE2NTk5NDg2MjE1ODcwMDI6MTUxMzI0ODY5NTA1MTplZDBkYzcwOTg3Y2M2MDcwNWJhMDBlMDY2YTk5MDJjNA; atkn=eyJhbGciOiAiUlNBLU9BRVAiLCAiZW5jIjogIkExMjhDQkMtSFMyNTYiLCAia2lkIjogInVub18xIn0.L9Ss-VwI0yaFq3_LaIqjqc7BTbiwsCkoqDQC22KfgUnBZxwSyJIIgp9c-TtuSR9CNDOf_S6qQY8AVK7Pmrdqly-65GUxBYRUCselvkHSu1cO_hf3iBtVDQsFPvsK-xABqiiN2j4CLD3wMPLtTLpajBoxVFUGLs4xrhkj5YAIAx0.KPf6KFhqruXL2wrhDXZgyg.KG5q24pOBX4piP6shzFIOo3DG4LQ1RuwJAneqtvYgiHpfBl7Hm7kHyzhtRS9N5gJdDDVc5KCIo94sVfpzE_46_b6A0iEBPHmsl0Y3TRS2s97VyOpxLMMEDc-99FyTPlmPRdjryfmf7KiS74tKZXULBp0v085DbyVc1ENo2fxPlIVJCdzyjMd4vCC_qAyF3vFQHcAZPlHTDSmnO8J-ewerJXrbaPJopdoOQ7q_kgaZ9BFOIYgf9mAf4vBhi6dIkGn4iKKsdqHDfc9E2uzDcrwJg.KRKLn9jEPBOnMhav9z7ZEg; rtkn=eyJhbGciOiAiUlNBLU9BRVAiLCAiZW5jIjogIkExMjhDQkMtSFMyNTYiLCAia2lkIjogInVub18xIn0.GwDf9rS2kdczP-72HhzXA_e2y3OAnw19Lg4coW1v7ans31TQJDDTUPDQiKy9vuZffS3HndZ9KeVf2j-SbpArmqd72v56USOUAZcs4TrJ1A-7WknFOmJBlIjSGAfAgrvA32C6zZ80kYVzxXLTSE3B8x_CmEgwV-R98CgHe87wNPo.J6DIkDXompDc1OtNJ52qyw.-CmV0by7AaLODshIVKpqGMbiSW2qniORCKnuIQhn9380kmqK_WVS3nTvZPSUJMPGIG4oUVdTm87wreP_DKPfW23tLW7WLRxvONT0_P-L24yndFLr66YW3T0wKu6EUT9Kvu9mEE9AiicK-j2Cl5EJcKJMIxcRYlIzCVxDGoTBluTd7Y4nBsr9cpmR6yF0nafXofjNEJompwVt60m6LkDSIoy1kG65uMpxZyYmBsVkwLqLHhpyfDPboYrVPuuxih3zMHoKk_P0FPJGJzLDwMbU3w.qHqSK9WG8UaTZUFLtl-P4w; ACT_SSO_EVENT=LOGIN_SUCCESS:1512039095096; umbrellaId=3306746010444912014; s_umbrellaId=3306746010444912014; agegate=; country=; utkn=; urtkn=; facebookId=true; twitchId=true; twitterId=true; youTubeId=true'
 		},
-		  
-	};  
-	request(url, (err, response, body) => {    
-		var token = response.headers['set-cookie'][2].replace("XSRF-TOKEN=", "").replace("; Domain=callofduty.com; Path=/", "");    
-		console.log(token);  
+	};
+	request(url, (err, response, body) => {});
+}
+
+function getCSRF() {
+	var url = {
+		method: 'GET',
+		uri: "https://profile.callofduty.com/",
+		headers: {
+			'Content-Type': 'application/json'
+		},
+	};
+	request(url, (err, response, body) => {
+		var token = response.headers['set-cookie'][2].replace("XSRF-TOKEN=", "").replace("; Domain=callofduty.com; Path=/", "");
+		//fs.writeFile(__dirname + "/token.txt", token);
 	});
 }
 
-function getData() {
-	var url = {    
-		method: 'GET',
-		    uri: "https://profile.callofduty.com/cod/userInfo/MTQ0OTE2NTk5NDg2MjE1ODcwMDI6MTUxMzI0ODY5NTA1MTplZDBkYzcwOTg3Y2M2MDcwNWJhMDBlMDY2YTk5MDJjNA",
-		    headers: {      
+function openDrop(game, platform, gamertag) {
+	var settings = {
+		method: "POST",
+		type: 'json',
+		url: "https://my.callofduty.com/api/papi-client/inventory/v1/title/" + game + "/platform/" + platform + "/gamer/" + gamertag + "/item/CommonCrate/redeem",
+		headers: {
 			'Content-Type': 'application/json',
-			'Cookie': 'AMCV_0FB367C2524450B90A490D4C%40AdobeOrg=1099438348%7CMCIDTS%7C17501%7CMCMID%7C06274282484548674580096429307447731106%7CMCAAMLH-1512636251%7C6%7CMCAAMB-1512643887%7CRKhpRz8krg2tLO6pguXWp5olkAcUniQYPHaMWWgdJ3xzPWQmdj0y%7CMCOPTOUT-1512046287s%7CNONE%7CMCAID%7CNONE%7CMCSYNCSOP%7C411-17506%7CvVersion%7C2.1.0; mbox=PC#7b3f20e658be47a393e4606c18564e9b.26_14#1519815088|session#a5b8dde9ef63450b8abdc7c55440c6dc#1512040948|check#true#1512039148; s_nr=1512039089070-Repeat; ACT_SSO_REMEMBER_ME=MTQ0OTE2NTk5NDg2MjE1ODcwMDI6MTZmNDhlYzg2NzRkMzkxNmIxNzQyZmM4OWNjY2NlMDM0ZTU3MTdkNDFhYjZjYjZiZjU4YzY0ZGYwYzU0N2E3MQ; CRM_BLOB=eyJ2ZXIiOjEsInBsYXQiOnt9fQ; __utma=229673283.361720785.1511957272.1511967999.1512031449.5; __utmz=229673283.1511957272.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); ATVI_VISITOR_ID=anon-1511959111787-0.8013522209195922; atvi-cookie=true; AMCVS_0FB367C2524450B90A490D4C%40AdobeOrg=1; ACT_SSO_LOCALE=en_US; __utmc=229673283; s_ppvl=https%253A%2F%2Fprofile.callofduty.com%2Fcod%2Flogin%2C74%2C74%2C978%2C1920%2C978%2C1920%2C1080%2C1%2CL; s_ppv=https%253A%2F%2Fprofile.callofduty.com%2Fcod%2Flogin%253FredirectUrl%253Dhttps%25253A%25252F%25252Fmy.callofduty.com%25252Fwwii%25252Fstats%25252Flifetime%2C74%2C74%2C978%2C1920%2C978%2C1920%2C1080%2C1%2CL; s_cc=true; s_sq=%5B%5BB%5D%5D; new_SiteId=cod; comid=cod; XSRF-TOKEN=9e9d114c-c67a-443d-92ca-c5273789dc84; s_dfa=activision.prd; gpv_c8=no%20value; redirectUrl=https://my.callofduty.com/wwii/stats/lifetime; SSO_REDIRECTED_AT_LOGIN=https://my.callofduty.com/wwii/stats/lifetime; ACT_SSO_COOKIE=MTQ0OTE2NTk5NDg2MjE1ODcwMDI6MTUxMzI0ODY5NTA1MTplZDBkYzcwOTg3Y2M2MDcwNWJhMDBlMDY2YTk5MDJjNA; s_ACT_SSO_COOKIE=MTQ0OTE2NTk5NDg2MjE1ODcwMDI6MTUxMzI0ODY5NTA1MTplZDBkYzcwOTg3Y2M2MDcwNWJhMDBlMDY2YTk5MDJjNA; atkn=eyJhbGciOiAiUlNBLU9BRVAiLCAiZW5jIjogIkExMjhDQkMtSFMyNTYiLCAia2lkIjogInVub18xIn0.L9Ss-VwI0yaFq3_LaIqjqc7BTbiwsCkoqDQC22KfgUnBZxwSyJIIgp9c-TtuSR9CNDOf_S6qQY8AVK7Pmrdqly-65GUxBYRUCselvkHSu1cO_hf3iBtVDQsFPvsK-xABqiiN2j4CLD3wMPLtTLpajBoxVFUGLs4xrhkj5YAIAx0.KPf6KFhqruXL2wrhDXZgyg.KG5q24pOBX4piP6shzFIOo3DG4LQ1RuwJAneqtvYgiHpfBl7Hm7kHyzhtRS9N5gJdDDVc5KCIo94sVfpzE_46_b6A0iEBPHmsl0Y3TRS2s97VyOpxLMMEDc-99FyTPlmPRdjryfmf7KiS74tKZXULBp0v085DbyVc1ENo2fxPlIVJCdzyjMd4vCC_qAyF3vFQHcAZPlHTDSmnO8J-ewerJXrbaPJopdoOQ7q_kgaZ9BFOIYgf9mAf4vBhi6dIkGn4iKKsdqHDfc9E2uzDcrwJg.KRKLn9jEPBOnMhav9z7ZEg; rtkn=eyJhbGciOiAiUlNBLU9BRVAiLCAiZW5jIjogIkExMjhDQkMtSFMyNTYiLCAia2lkIjogInVub18xIn0.GwDf9rS2kdczP-72HhzXA_e2y3OAnw19Lg4coW1v7ans31TQJDDTUPDQiKy9vuZffS3HndZ9KeVf2j-SbpArmqd72v56USOUAZcs4TrJ1A-7WknFOmJBlIjSGAfAgrvA32C6zZ80kYVzxXLTSE3B8x_CmEgwV-R98CgHe87wNPo.J6DIkDXompDc1OtNJ52qyw.-CmV0by7AaLODshIVKpqGMbiSW2qniORCKnuIQhn9380kmqK_WVS3nTvZPSUJMPGIG4oUVdTm87wreP_DKPfW23tLW7WLRxvONT0_P-L24yndFLr66YW3T0wKu6EUT9Kvu9mEE9AiicK-j2Cl5EJcKJMIxcRYlIzCVxDGoTBluTd7Y4nBsr9cpmR6yF0nafXofjNEJompwVt60m6LkDSIoy1kG65uMpxZyYmBsVkwLqLHhpyfDPboYrVPuuxih3zMHoKk_P0FPJGJzLDwMbU3w.qHqSK9WG8UaTZUFLtl-P4w; ACT_SSO_EVENT=LOGIN_SUCCESS:1512039095096; umbrellaId=3306746010444912014; s_umbrellaId=3306746010444912014; agegate=; country=; utkn=; urtkn=; facebookId=true; twitchId=true; twitterId=true; youTubeId=true'    
+			'Cookie': 'AMCV_0FB367C2524450B90A490D4C%40AdobeOrg=1099438348%7CMCIDTS%7C17501%7CMCMID%7C06274282484548674580096429307447731106%7CMCAAMLH-1512636251%7C6%7CMCAAMB-1512643887%7CRKhpRz8krg2tLO6pguXWp5olkAcUniQYPHaMWWgdJ3xzPWQmdj0y%7CMCOPTOUT-1512046287s%7CNONE%7CMCAID%7CNONE%7CMCSYNCSOP%7C411-17506%7CvVersion%7C2.1.0; mbox=PC#7b3f20e658be47a393e4606c18564e9b.26_14#1519815088|session#a5b8dde9ef63450b8abdc7c55440c6dc#1512040948|check#true#1512039148; s_nr=1512039089070-Repeat; ACT_SSO_REMEMBER_ME=MTQ0OTE2NTk5NDg2MjE1ODcwMDI6MTZmNDhlYzg2NzRkMzkxNmIxNzQyZmM4OWNjY2NlMDM0ZTU3MTdkNDFhYjZjYjZiZjU4YzY0ZGYwYzU0N2E3MQ; CRM_BLOB=eyJ2ZXIiOjEsInBsYXQiOnt9fQ; __utma=229673283.361720785.1511957272.1511967999.1512031449.5; __utmz=229673283.1511957272.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); ATVI_VISITOR_ID=anon-1511959111787-0.8013522209195922; atvi-cookie=true; AMCVS_0FB367C2524450B90A490D4C%40AdobeOrg=1; ACT_SSO_LOCALE=en_US; __utmc=229673283; s_ppvl=https%253A%2F%2Fprofile.callofduty.com%2Fcod%2Flogin%2C74%2C74%2C978%2C1920%2C978%2C1920%2C1080%2C1%2CL; s_ppv=https%253A%2F%2Fprofile.callofduty.com%2Fcod%2Flogin%253FredirectUrl%253Dhttps%25253A%25252F%25252Fmy.callofduty.com%25252Fwwii%25252Fstats%25252Flifetime%2C74%2C74%2C978%2C1920%2C978%2C1920%2C1080%2C1%2CL; s_cc=true; s_sq=%5B%5BB%5D%5D; new_SiteId=cod; comid=cod; XSRF-TOKEN=9e9d114c-c67a-443d-92ca-c5273789dc84; s_dfa=activision.prd; gpv_c8=no%20value; redirectUrl=https://my.callofduty.com/wwii/stats/lifetime; SSO_REDIRECTED_AT_LOGIN=https://my.callofduty.com/wwii/stats/lifetime; ACT_SSO_COOKIE=MTQ0OTE2NTk5NDg2MjE1ODcwMDI6MTUxMzI0ODY5NTA1MTplZDBkYzcwOTg3Y2M2MDcwNWJhMDBlMDY2YTk5MDJjNA; s_ACT_SSO_COOKIE=MTQ0OTE2NTk5NDg2MjE1ODcwMDI6MTUxMzI0ODY5NTA1MTplZDBkYzcwOTg3Y2M2MDcwNWJhMDBlMDY2YTk5MDJjNA; atkn=eyJhbGciOiAiUlNBLU9BRVAiLCAiZW5jIjogIkExMjhDQkMtSFMyNTYiLCAia2lkIjogInVub18xIn0.L9Ss-VwI0yaFq3_LaIqjqc7BTbiwsCkoqDQC22KfgUnBZxwSyJIIgp9c-TtuSR9CNDOf_S6qQY8AVK7Pmrdqly-65GUxBYRUCselvkHSu1cO_hf3iBtVDQsFPvsK-xABqiiN2j4CLD3wMPLtTLpajBoxVFUGLs4xrhkj5YAIAx0.KPf6KFhqruXL2wrhDXZgyg.KG5q24pOBX4piP6shzFIOo3DG4LQ1RuwJAneqtvYgiHpfBl7Hm7kHyzhtRS9N5gJdDDVc5KCIo94sVfpzE_46_b6A0iEBPHmsl0Y3TRS2s97VyOpxLMMEDc-99FyTPlmPRdjryfmf7KiS74tKZXULBp0v085DbyVc1ENo2fxPlIVJCdzyjMd4vCC_qAyF3vFQHcAZPlHTDSmnO8J-ewerJXrbaPJopdoOQ7q_kgaZ9BFOIYgf9mAf4vBhi6dIkGn4iKKsdqHDfc9E2uzDcrwJg.KRKLn9jEPBOnMhav9z7ZEg; rtkn=eyJhbGciOiAiUlNBLU9BRVAiLCAiZW5jIjogIkExMjhDQkMtSFMyNTYiLCAia2lkIjogInVub18xIn0.GwDf9rS2kdczP-72HhzXA_e2y3OAnw19Lg4coW1v7ans31TQJDDTUPDQiKy9vuZffS3HndZ9KeVf2j-SbpArmqd72v56USOUAZcs4TrJ1A-7WknFOmJBlIjSGAfAgrvA32C6zZ80kYVzxXLTSE3B8x_CmEgwV-R98CgHe87wNPo.J6DIkDXompDc1OtNJ52qyw.-CmV0by7AaLODshIVKpqGMbiSW2qniORCKnuIQhn9380kmqK_WVS3nTvZPSUJMPGIG4oUVdTm87wreP_DKPfW23tLW7WLRxvONT0_P-L24yndFLr66YW3T0wKu6EUT9Kvu9mEE9AiicK-j2Cl5EJcKJMIxcRYlIzCVxDGoTBluTd7Y4nBsr9cpmR6yF0nafXofjNEJompwVt60m6LkDSIoy1kG65uMpxZyYmBsVkwLqLHhpyfDPboYrVPuuxih3zMHoKk_P0FPJGJzLDwMbU3w.qHqSK9WG8UaTZUFLtl-P4w; ACT_SSO_EVENT=LOGIN_SUCCESS:1512039095096; umbrellaId=3306746010444912014; s_umbrellaId=3306746010444912014; agegate=; country=; utkn=; urtkn=; facebookId=true; twitchId=true; twitterId=true; youTubeId=true'
 		},
-		  
-	};  
-	request(url, (err, response, body) => {    
-		console.log(body);  
-	});
+	};
+	try {
+		request(settings, function(response, status, body) {
+			var json = JSON.parse(body);
+			//fs.writeFile(__dirname + '/body.json', JSON.stringify(json.data, null, 8));
+		});
+	} catch (exception) {
+	}
 }
